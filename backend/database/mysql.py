ccdd7caf88d5f90ec6a0b388d16bf3a81ad209d3
Lanzhijiang <lanzhijiang@foxmail.com>
2020-10-25 00:30:33 +0800

    「添加」mysql数据库操作类

---
+++ backend/database/mysql.py
@@ -0,0 +1,31 @@
+# coding=utf-8
+# author: Lan_zhijiang
+# desciption: mysql操作类
+# date: 2020/10/25
+
+import pymysql
+
+
+class GtdMySqlManipulator():
+
+    def __init__(self, log, setting, database_name="default"):
+
+        self.log = log
+        self.setting = setting
+        self.database_name = database_name
+        self.mysql_settings = self.setting["databaseSettings"]["mysql"]
+
+        try:
+            self.mysql_server_address = self.mysql_settings["address"][database_name]
+        except KeyError:
+            self.log.add_log("MysqlManipulator: Can't find mysql server named: "
+                             + database_name + "'s address in the settings, please check it.", 3)
+        else:
+            self.db = pymysql.connect(host=self.mysql_server_address,
+                                      user=self.mysql_settings["user"][database_name],
+                                      password=self.mysql_settings["password"][database_name],
+                                      port=self.mysql_settings["port"][database_name],
+                                      database=self.database_name,
+                                      charset="utf-8")
+
+            self.cursor = self.db.cursor()
