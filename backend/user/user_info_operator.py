e79902671c1a4063622ef03f352badd402e1ad5e
Lanzhijiang <lanzhijiang@foxmail.com>
2020-11-01 10:21:44 +0800

    「更新」用户信息操作器（func迁移）

--- backend/user/user_info_operator.py
+++ backend/user/user_info_operator.py
@@ -4,6 +4,7 @@
 # date: 2020/10/17
 
 from backend.user.user_manager import UserManager
+from backend.database.memcached import MemcachedManipulator
 
 
 class GtdUerInfoManager():
@@ -11,11 +12,56 @@ class GtdUerInfoManager():
     def __init__(self, account, log, setting):
 
         self.gtd_user_manager = UserManager(log, setting)
+        self.memcached_manipulator = MemcachedManipulator(log, setting)
         self.log = log
         self.setting = setting
 
         self.account = account
-        self.account_info = self.gtd_user_manager.get_user_info([account])
+        self.account_info = self.get_user_info([account])
+
+    def update_user_info(self, account, info):
+
+        """
+        更新用户信息
+        :param account: 账户名
+        :param info: 要更新的信息
+        :return bool
+        """
+        if type(info) != dict:
+            self.log.add_log("UserInfoManager: Failed to update user info: info must be a dict", 3)
+            return False
+
+        user_info = self.memcached_manipulator._get(account)
+
+        for now_key in info.keys():
+            try:
+                self.log.add_log("UserManager: Updating " + str(now_key) + "'s info", 1)
+                user_info[now_key] = info[now_key]
+            except KeyError:
+                self.log.add_log("User Manager: can't find " + str(now_key) + " in user_info, skip!", 3)
+
+        self.memcached_manipulator._replace(account, user_info)
+        return True
+
+    def get_user_info(self, accounts):
+
+        """
+        获取用户信息（可多个）
+        :param accounts: 账户名 list
+        :return dict
+        """
+        if type(accounts) != list:
+            self.log.add_log("UserInfoManager: Param 'account' must be a list!", 3)
+            return False
+
+        user_info = self.memcached_manipulator._get_multi(accounts)
+
+        for account in accounts:
+            self.log.add_log("UserManager: Getting " + str(account) + "'s info", 1)
+            if user_info[account] is None:
+                self.log.add_log("UserManager: Can't find " + str(account), 3)
+
+        return user_info
 
     def reset_param(self, account):
 
@@ -28,7 +74,7 @@ class GtdUerInfoManager():
             account = self.account
             account_info = self.account_info
         else:
-            account_info = self.gtd_user_manager.get_user_info([account])
+            account_info = self.get_user_info([account])
 
         return account, account_info
 
@@ -47,7 +93,7 @@ class GtdUerInfoManager():
         account_info[key] = value
         self.account_info = account_info
 
-        return self.gtd_user_manager.update_user_info(account, account_info)
+        return self.update_user_info(account, account_info)
 
     def get_info(self, key, account=None):
 
