add61064456c7fa80a895c9270e035a3d0f44d5b
Lanzhijiang <lanzhijiang@foxmail.com>
2020-11-22 13:15:07 +0800

    「修复」sign_up

--- backend/user/user_manager.py
+++ backend/user/user_manager.py
@@ -9,7 +9,7 @@ from backend.database.mongodb import MongoDBManipulator
 from backend.user.user_group_manager import UserGroupManager
 
 
-class UserManager():
+class UserManager:
 
     def __init__(self, log, setting):
 
@@ -34,24 +34,21 @@ class UserManager():
             self.log.add_log("UserManager: '/', '.' and '-' is banned in account name", 3)
             return False
 
-        user_info = json.load(open("./backend/data/json/user_info_template.json", "r", encoding="utf-8"))
-        user_info["_id"] = 0
-        user_info["account"] = str(account)
-        user_info["password"] = str(password)
-        user_info["email"][0] = email
-        user_info["userGroup"] = user_group
-
         self.user_group_manager.add_user_in(account, user_group)
 
         if self.mongodb_manipulator.add_collection("user", account) is False:
-            self.log.add_log("UserManager: Sign up failed, this user had already exists. user: " + account, 3)
+            self.log.add_log("UserManager: Sign up failed, this user had already exists. sign up account: " + account, 3)
             return False
         else:
             self.log.add_log("UserManager: Account add to the collection: user successfully", 1)
 
-        if self.mongodb_manipulator.add_one_document("user", account, user_info) is False:
-            self.log.add_log("UserManager: Sign up failed, something went wrong while adding document. account: "
-                             + account, 3)
+        user_info = json.load(open("./data/json/user_info_template.json", "r", encoding="utf-8"))
+        user_info[0]["account"] = account
+        user_info[1]["password"] = password
+        user_info[2]["email"].append(email)
+        user_info[4]["userGroup"] = user_group
+        if self.mongodb_manipulator.add_many_documents("user", account, user_info) is False:
+            self.log.add_log("UserManager: Sign up failed, something went wrong. sign up account: " + account, 3)
             return False
         else:
             self.log.add_log("UserManager: Sign up success", 1)
@@ -87,7 +84,7 @@ class UserManager():
 
                 self.mongodb_manipulator.update_many_documents("user", account, {"_id": 0}, {"token": token})
                 self.setting["user"]["account"] = account
-                self.setting["user"]["avatar"] = user_info["avatar"]
+                self.setting["user"]["avatar"] = user_info["avatar"]  # needs a solution
 
                 # add user group manager to get permission
 
