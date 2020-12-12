73aec9244bf6b0ed98b74d8fb2ff78b02246b74e
Lanzhijiang <lanzhijiang@foxmail.com>
2020-12-13 00:09:38 +0800

    「更新」添加http_handler

---
+++ backend/api/http/http_handler.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+# author: Lan_zhijiang
+# description: http handler
+# date: 2020/12/12
+
+
+class HttpHandler:
+
+    def __init__(self, log, setting):
+
+        self.log = log
+        self.setting = setting
