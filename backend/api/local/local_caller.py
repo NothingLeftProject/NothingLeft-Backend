d2196edc08385d7fcfb2df3f3c9a26c88a782fff
Lanzhijiang <lanzhijiang@foxmail.com>
2020-12-13 19:00:54 +0800

    「更新」添加了许多的函数

--- backend/api/local/local_caller.py
+++ backend/api/local/local_caller.py
@@ -4,6 +4,8 @@
 # date: 2020/11/15
 
 from backend.user.user_manager import UserManager
+from backend.user.user_info_operator import UserInfoManager
+from backend.user.permission_manager import UserPermissionManager
 
 
 class LocalCaller:
@@ -14,6 +16,8 @@ class LocalCaller:
         self.setting = setting
 
         self.user_manager = UserManager(log, setting)
+        self.user_permission_manager = UserPermissionManager(log, setting)
+        self.user_info_manager = UserInfoManager(log, setting)
 
     def user_login(self, param):
 
@@ -61,6 +65,99 @@ class LocalCaller:
             if res is False:
                 return False, err
             else:
-                result["status"] = "success"
+                return result
+        
+    def user_get_permissions(self, param):
+
+        """
+        获取用户权限
+        :return:
+        """
+        self.log.add_log("LocalCaller: start user_get_permission", 1)
+
+        result = {}
+        try:
+            account = param["account"]
+        except KeyError:
+            self.log.add_log("LocalCaller: user_get_permission: Your param is incomplete", 3)
+            return False, "param incomplete"
+        else:
+            try:
+                is_cache = param["isCache"]
+                is_update = param["isUpdate"]
+            except KeyError:
+                is_cache = True
+                is_update = False
+            
+            res, err = self.user_permission_manager.get_user_permissions(account, cache_to_memcached=is_cache, ask_update=is_update)
+            if res is False:
+                return False, err
+            else:
+                result["permissionList"] = res
+                return result
+    
+    def user_delete(self, param):
+
+        """
+        删除用户
+        :return:
+        """
+        self.log.add_log("LocalCaller: start user_delete", 1)
+        
+        result = {}
+        try:
+            account = param["account"]
+        except KeyError:
+            self.log.add_log("LocalCaller: user_delete: Your param is incomplete", 3)
+            return False, "param incomplete"
+        else:
+            res, err = self.user_manager.delete_user(account)
+            if res is False:
+                return False, err
+            else:
+                return result
+
+    def user_info_update(self, param):
+
+        """
+        更新用户信息
+        :return:
+        """
+        self.log.add_log("LocalCaller: start user_info_update", 1)
+
+        result = {}
+        try:
+            account = param["account"]
+            info = param["info"]
+        except KeyError:
+            self.log.add_log("LocalCaller: user_info_update: Your param is incomplete", 3)
+            return False, "param incomplete"
+        else:
+            res, err = self.user_info_manager.update_user_info(account, info)
+            if res is False:
+                return res, err
+            else:
+                return result
+
+    def user_info_get_all(self, param):
+
+        """
+        获取用户所有信息
+        :return:
+        """
+        self.log.add_log("LocalCaller: start user_info_get_all", 1)
+
+        result = {}
+        try:
+            accounts = param["accounts"]
+        except KeyError:
+            self.log.add_log("LocalCaller: user_info_get_all: Your param is incomplete", 3)
+            return False, "param incomplete, attention, it's 'accounts'"
+        else:
+            res, err = self.user_info_manager.get_users_all_info(accounts)
+            if res is False:
+                return res, err
+            else:
+                result["usersInfo"] = res
                 return result
 
