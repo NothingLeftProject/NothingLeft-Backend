c7406053a0513ad720e9b3891d3249261531b046
Lanzhijiang <lanzhijiang@foxmail.com>
2020-12-13 13:59:53 +0800

    「添加」command finder
    command finder 是一个方便http handler找到指令对应的处理函数的类

---
+++ backend/api/http/command_finder.py
@@ -0,0 +1,17 @@
+# coding=utf-8
+# author: Lan_zhijiang
+# description: http command finder
+# date: 2020/12/13 (1937.12.13 勿忘国耻，铭记历史)
+
+from backend.api.local.local_caller import LocalCaller
+
+
+class CommandFinder:
+
+    def __init__(self, log, setting):
+
+        self.local_caller = LocalCaller(log, setting)
+
+        self.all_command_list = {
+
+        }
