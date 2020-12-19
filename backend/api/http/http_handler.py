ba13a5e871cb640c51381392051ba7520f1d82c2
Lanzhijiang <lanzhijiang@foxmail.com>
2020-12-20 00:40:32 +0800

    「修复」账户判断错误

--- backend/api/http/http_handler.py
+++ backend/api/http/http_handler.py
@@ -34,46 +34,41 @@ class HttpHandler:
         self.log.add_log("HttpHandler: start auth", 1)
         # is the user exists
         account = self.request_data["header"]["account"]
-        try:
-            if "@" in account or str(int(account)) == account:  # ATTENTION: error might be here
-                now_time_stamp = self.log.get_time_stamp()
-                gave_time_stamp = self.request_data["header"]["timeStamp"]
-                time_loss = int(now_time_stamp) - int(gave_time_stamp)
+        if "@" in account or str(account) == account:  # ATTENTION: error might be here
+            now_time_stamp = self.log.get_time_stamp()
+            gave_time_stamp = self.request_data["header"]["timeStamp"]
+            time_loss = int(now_time_stamp) - int(gave_time_stamp)
 
-                # is time stamp in law
-                if time_loss > 0 and time_loss < 60:
-                    last_login_time_stamp = \
+            # is time stamp in law
+            if time_loss > 0 and time_loss < 60:
+                last_login_time_stamp = \
                     self.mongodb_mainpulator.get_document("user", account, query={"_id": 13}, mode=2)[
                         "lastLoginTimeStamp"]
-                    login_time_loss = gave_time_stamp - last_login_time_stamp
-                    if login_time_loss > 0 and login_time_loss < 3600 * 24:
-                        self.log.add_log("HttpHandler: time stamp is in law", 1)
+                login_time_loss = gave_time_stamp - last_login_time_stamp
+                if login_time_loss > 0 and login_time_loss < 3600 * 24:
+                    self.log.add_log("HttpHandler: time stamp is in law", 1)
 
-                        # is token same
-                        real_token = self.mongodb_mainpulator.get_document("user", account, query={"_id": 7}, mode=2)[
-                            "token"]
-                        need_verify_token = self.response_data["header"]["token"]
-                        if real_token == need_verify_token:
-                            # auth pass, load permission list
-                            self.permission_list = self.permission_manager.get_user_permissions(account)
-                            if self.permission_list is False:
-                                self.log.add_log("HttpHandler: can't load permission list", 3)
-                                self.response_data["header"][
-                                    "errorMsg"] = "database or something wrong with the backend"  # inside error
-                                return False
-                            return True
-                    else:
-                        self.log.add_log("HttpHandler: login outdate", 1)
-                        self.response_data["header"]["errorMsg"] = "login outdate, please login"  # login outdate error
+                    # is token same
+                    real_token = self.mongodb_mainpulator.get_document("user", account, query={"_id": 7}, mode=2)[
+                        "token"]
+                    need_verify_token = self.response_data["header"]["token"]
+                    if real_token == need_verify_token:
+                        # auth pass, load permission list
+                        self.permission_list = self.permission_manager.get_user_permissions(account)
+                        if self.permission_list is False:
+                            self.log.add_log("HttpHandler: can't load permission list", 3)
+                            self.response_data["header"][
+                                "errorMsg"] = "database or something wrong with the backend"  # inside error
+                            return False
+                        return True
                 else:
-                    self.log.add_log("HttpHandler: time stamp not in law", 1)
-                    self.response_data["header"]["errorMsg"] = "time stamp is not in law"  # timestamp error
-                    return False
+                    self.log.add_log("HttpHandler: login outdate", 1)
+                    self.response_data["header"]["errorMsg"] = "login outdate, please login"  # login outdate error
             else:
-                self.log.add_log("HttpHandler: account not exists or format error", 1)
-                self.response_data["header"]["errorMsg"] = "user does not exists or format error"  # user not exists error
+                self.log.add_log("HttpHandler: time stamp not in law", 1)
+                self.response_data["header"]["errorMsg"] = "time stamp is not in law"  # timestamp error
                 return False
-        except ValueError:
+        else:
             self.log.add_log("HttpHandler: account not exists or format error", 1)
             self.response_data["header"]["errorMsg"] = "user does not exists or format error"  # user not exists error
             return False
