27d6c3e1bce70f7f2ef19e2bf132798cdd4a9ebb
Lanzhijiang <lanzhijiang@foxmail.com>
2021-01-02 16:06:39 +0800

    「更新&修复」一堆bug，可以完成需登录后的命令了

--- backend/api/http/http_handler.py
+++ backend/api/http/http_handler.py
@@ -24,6 +24,7 @@ class HttpHandler:
         self.request_data = {}
         self.permission_list = []
         self.response_data = json.load(open("./backend/data/json/response_template.json", "r", encoding="utf-8"))
+        self.special_auth_pass = False
 
     def auth(self):
 
@@ -50,18 +51,30 @@ class HttpHandler:
             # is time stamp in law
             if 0 <= time_loss <= 600:
 
-                if self.request_data["header"]["loginRequest"]:
-                    return True
-                elif self.request_data["header"]["signupRequest"]:
-                    return True
+                try:
+                    if self.request_data["header"]["loginRequest"]:
+                        self.special_auth_pass = True
+                        return True
+                    elif self.request_data["header"]["signupRequest"]:
+                        self.special_auth_pass = True
+                        return True
+                except KeyError:
+                    pass
 
                 last_login_time_stamp = self.mongodb_mainpulator.parse_document_result(
                     self.mongodb_mainpulator.get_document("user", account, {"lastLoginTimeStamp": 1}, 2),
                     ["lastLoginTimeStamp"]
                 )[0]["lastLoginTimeStamp"]
-                print(last_login_time_stamp)
 
-                login_time_loss = abs(int(gave_time_stamp) - int(last_login_time_stamp))
+                try:
+                    login_time_loss = abs(int(gave_time_stamp) - int(last_login_time_stamp))
+                except TypeError:
+                    self.log.add_log("HttpHandler: user-" + account + " haven't login for once yet", 1)
+                    self.response_data["header"]["errorMsg"] = "user-" + account + " haven't login for once yet"
+                    return False
+                else:
+                    self.log.add_log("HttpHandler: user-" + account + "'s LLTS: " + last_login_time_stamp, 1)
+
                 if 0 <= login_time_loss <= 3600 * 24:
                     self.log.add_log("HttpHandler: time stamp is in law", 1)
 
@@ -70,14 +83,14 @@ class HttpHandler:
                         self.mongodb_mainpulator.get_document("user", account, {"token": 1}, 2),
                         ["token"]
                     )[0]["token"]
-                    need_verify_token = self.response_data["header"]["token"]
+                    need_verify_token = self.request_data["header"]["token"]
                     if real_token == need_verify_token:
                         # auth pass, load permission list
-                        self.permission_list = self.permission_manager.get_user_permissions(account)
+                        self.log.add_log("HttpHandler: token compared. load permissions list", 1)
+                        self.permission_list, err = self.permission_manager.get_user_permissions(account, ask_update=True)
                         if self.permission_list is False:
                             self.log.add_log("HttpHandler: can't load permission list", 3)
-                            self.response_data["header"][
-                                "errorMsg"] = "database or something wrong with the backend"  # inside error
+                            self.response_data["header"]["errorMsg"] = "database or something wrong with the backend"  # inside error
                             return False
                         return True
                 else:
@@ -105,47 +118,48 @@ class HttpHandler:
 
         if self.auth():
             self.log.add_log("HttpHandler: auth completed", 1)
-            special_pass = False
+            special_handle_pass = False
 
-            # the handle of login command
-            if self.request_data["header"]["loginRequest"]:
-                try:
-                    command_name = self.request_data["command"][0]["commandName"]
-                except IndexError:
-                    self.response_data["header"]["status"] = 1
-                    self.response_data["header"]["errorMsg"] = "you lied to me! you are not here to login!"
-                    self.log.add_log("HttpHandler: can't find commandName", 1)
-                else:
-                    if command_name != "user_login":
-                        self.response_data["header"]["status"] = 1
-                        self.response_data["header"]["errorMsg"] = "you lied to me! you are not here to login!"
-                        self.log.add_log("HttpHandler: false request to login", 1)
-                    else:
-                        self.response_data["header"]["status"] = 0
-                        self.response_data["header"]["errorMsg"] = None
-                        special_pass = True
-            # the handle of signup request
-            elif self.request_data["header"]["signupRequest"]:
-                if self.setting["allowSignup"]:
+            if self.special_auth_pass:
+                # the handle of login request
+                if self.request_data["header"]["loginRequest"]:
                     try:
                         command_name = self.request_data["command"][0]["commandName"]
                     except IndexError:
                         self.response_data["header"]["status"] = 1
-                        self.response_data["header"]["errorMsg"] = "you lied to me! you are not here to sign up!"
+                        self.response_data["header"]["errorMsg"] = "you lied to me! you are not here to login!"
                         self.log.add_log("HttpHandler: can't find commandName", 1)
                     else:
-                        if command_name != "user_sign_up":
+                        if command_name != "user_login":
                             self.response_data["header"]["status"] = 1
-                            self.response_data["header"]["errorMsg"] = "you lied to me! you are not here to sign up!"
-                            self.log.add_log("HttpHandler: false request to sign up", 1)
+                            self.response_data["header"]["errorMsg"] = "you lied to me! you are not here to login!"
+                            self.log.add_log("HttpHandler: false request to login", 1)
                         else:
                             self.response_data["header"]["status"] = 0
                             self.response_data["header"]["errorMsg"] = None
-                            special_pass = True
-                else:
-                    self.response_data["header"]["status"] = 1
-                    self.response_data["header"]["errorMsg"] = "not allow sign up free, please contact your admin"
-                    self.log.add_log("HttpHandler: not allow sign up free", 1)
+                            special_handle_pass = True
+                # the handle of signup request
+                elif self.request_data["header"]["signupRequest"]:
+                    if self.setting["allowSignup"]:
+                        try:
+                            command_name = self.request_data["command"][0]["commandName"]
+                        except IndexError:
+                            self.response_data["header"]["status"] = 1
+                            self.response_data["header"]["errorMsg"] = "you lied to me! you are not here to sign up!"
+                            self.log.add_log("HttpHandler: can't find commandName", 1)
+                        else:
+                            if command_name != "user_sign_up":
+                                self.response_data["header"]["status"] = 1
+                                self.response_data["header"]["errorMsg"] = "you lied to me! you are not here to sign up!"
+                                self.log.add_log("HttpHandler: false request to sign up", 1)
+                            else:
+                                self.response_data["header"]["status"] = 0
+                                self.response_data["header"]["errorMsg"] = None
+                                special_handle_pass = True
+                    else:
+                        self.response_data["header"]["status"] = 1
+                        self.response_data["header"]["errorMsg"] = "not allow sign up free, please contact your admin"
+                        self.log.add_log("HttpHandler: not allow sign up free", 1)
 
             # the handle of normal command
             try:
@@ -169,7 +183,7 @@ class HttpHandler:
 
                     command_response["commandName"] = command_name
 
-                    if command_name in self.permission_list or special_pass is True:
+                    if command_name in self.permission_list or special_handle_pass is True:
                         self.log.add_log("HttpHandler: " + command_name + " is allowed, start handle", 1)
                         try:
                             command_handle_function = self.command_finder.all_command_list[command_name]
