1a0dd9b79cc9053c85a8578673b50117942ed8e2
xiaoland <lanzhijiang@foxmail.com>
2021-02-27 23:23:43 +0800

    「更新」...

--- backend/core/gtd/classification_manager.py
+++ backend/core/gtd/classification_manager.py
@@ -10,3 +10,37 @@ class ClassifyManager():
 
         self.log = log
         self.setting = setting
+
+    def add_many_stuffs(self, account, stuff_ids, classification_name):
+
+        """
+        添加多个stuffs到某个分类里
+        :param account: 用户名
+        :param stuff_ids:
+        :param classification_name: 分类表名 str 或 int
+        :type stuff_ids: list
+        :return: bool, str
+        """
+
+    def remove_many_stuffs(self, account, stuff_ids, classification_name):
+
+        """
+        从分类中移除多个stuffs
+        :param account: 用户名
+        :param stuff_ids:
+        :param classification_name: 分类表名 str 或 int
+        :return: bool, str
+        """
+
+    def add_classification(self, account, classification_name, classification_id):
+
+        """
+        添加一个新的分类
+        :param account: 用户名
+        :param classification_name: 分类名 str
+        :param classification_id: 分类id int
+        :type classification_name: str
+        :type classification_id: int
+        :return: bool, str
+        """
+
