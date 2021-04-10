7f3fde4d3ff8bbdfc88a29167bee87537fd9bb2f
xiaoland <lanzhijiang@foxmail.com>
2021-04-11 00:47:12 +0800

    「更新」添加general_existence_judgment和add_many_stuffs

--- backend/core/gtd/classification_manager.py
+++ backend/core/gtd/classification_manager.py
@@ -19,6 +19,11 @@ class ClassificationManager():
         self.memcached_manipulator = MemcachedManipulator(log, setting)
         self.encryption = Encryption(self.log, self.setting)
 
+        self.standard_classification_info_keys = [
+            "name", "description", "stuffList", "stuffCount", "lastOperatedTimeStamp",
+            "classificationId", "type", "childClassificationList"
+        ]
+
     def is_cl_name_exist(self, account, cl_name):
 
         """
@@ -86,6 +91,24 @@ class ClassificationManager():
 
         return cl_id, True
 
+    def general_existence_judgment(self, account, cl_id):
+
+        """
+        通用存在判断：判断账户、分类是否存在
+        :param account:
+        :param cl_id:
+        :return: bool, str
+        """
+        if self.mongodb_manipulator.is_collection_exist("user", account) is False:
+            self.log.add_log("ClassificationManager: user-%s does not exist" % account, 3)
+            return False, "user-%s does not exist" % account
+
+        if self.is_cl_id_exist(account, cl_id) is False:
+            self.log.add_log("ClassificationManager: classification-%s does not exist" % cl_id, 3)
+            return False, "classification-%s does not exist" % cl_id
+
+        return True, "success"
+
     def add_many_stuffs(self, account, cl_id, stuff_ids):
 
         """
@@ -96,6 +119,43 @@ class ClassificationManager():
         :type stuff_ids: list
         :return: bool, str
         """
+        self.log.add_log("ClassificationManager: add many stuffs to user-%s's cl-%s" % (account, cl_id), 1)
+        skip_ids = []
+
+        # is param in law
+        if type(stuff_ids) != list:
+            self.log.add_log("ClassificationManager: param-stuff_ids must be a list, type error", 3)
+            return False, "ClassificationManager: param-stuff_ids must be a list, type error"
+
+        # general existence judgement
+        a, b = self.general_existence_judgment(account, cl_id)
+        if a is False:
+            return a, b
+
+        # main
+        cl_info = self.mongodb_manipulator.parse_document_result(
+            self.mongodb_manipulator.get_document("classification", account, {"_id": cl_id}, 1),
+            self.standard_classification_info_keys
+        )[0]
+        for stuff_id in stuff_ids:
+            if self.mongodb_manipulator.is_collection_exist("stuff", stuff_id) is False:
+                self.log.add_log("ClassificationManager: fail to add stuff-%s to cl-%s because it isn't exist", 3)
+                skip_ids.append(stuff_id)
+                continue
+            cl_info["stuffList"].append(stuff_id)
+
+        # update
+        if self.mongodb_manipulator.update_many_documents("classification", account, {"_id": cl_id}, cl_info) is False:
+            self.log.add_log("ClassificationManager: database error, can't add stuff into classification", 3)
+            return False, "database error"
+        else:
+            if skip_ids:
+                if skip_ids == stuff_ids:
+                    return False, "all stuff_id aren't exist at all"
+                else:
+                    return True, "but fail with stuffs-%s" % skip_ids
+            else:
+                return True, "success"
 
     def remove_many_stuffs(self, account, cl_id, stuff_ids):
 
