7349e56dbb9d0c5c83cccaea6327a0ace40d12b2
Lanzhijiang <lanzhijiang@foxmail.com>
2021-04-19 00:39:16 +0800

    「更新」减少资源开销第一步-添加BaseAbilities类

--- backend/init.py
+++ backend/init.py
@@ -3,7 +3,11 @@
 # description: backend init
 # date: 2020/12/12
 
+from backend.database.mongodb import MongoDBManipulator
+from backend.database.memcached import MemcachedManipulator
+from backend.data.encryption import Encryption
 from backend.data.log import GtdLog
+
 from backend.api.http.server import HttpServer
 from backend.core.maintain.maintainer import Maintainer
 
@@ -11,15 +15,25 @@ import json
 import threading
 
 
-class BackendInit:
-
+class BaseAbilities:
+    '''基础能力：log，MongoDB、MemcachedDB传递'''
     def __init__(self):
 
         self.log = GtdLog()
         self.setting = json.load(open("./backend/data/json/setting.json", "r", encoding="utf-8"))
 
-        self.Maintainer = Maintainer(self.log, self.setting)
-        self.http_server = HttpServer(self.log, self.setting)
+        self.mongodb_manipulator = MongoDBManipulator(self.log, self.setting)
+        self.memcached_manipulator = MemcachedManipulator(self.log, self.setting)
+        self.encryption = Encryption(self.log, self.setting)
+
+class BackendInit:
+
+    def __init__(self):
+
+        self.base_abilities = BaseAbilities()
+        
+        self.Maintainer = Maintainer(self.base_abilities, self.base_abilities.setting)
+        self.http_server = HttpServer(self.base_abilities, self.base_abilities.setting)
 
     def run_backend(self):
 
@@ -27,8 +41,8 @@ class BackendInit:
         启动后端
         :return 
         """
-        self.log.add_log("######## NL-BACKEND RUN NOW ########", 1)
-        self.log.add_log("BackendInit: now start backend", 1)
+        self.base_abilities.log.add_log("######## NL-BACKEND RUN NOW ########", 1)
+        self.base_abilities.log.add_log("BackendInit: now start backend", 1)
         
         thread_server = threading.Thread(target=self.http_server.run_server, args=())
         thread_maintainer = threading.Thread(target=self.Maintainer.run, args=())
